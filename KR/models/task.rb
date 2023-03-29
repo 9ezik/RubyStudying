bdbc05e0098b255d1e33ea94550b2f736e64682f
9ezik <ezik00747@gmail.com>
2023-03-29 22:01:13 +0300

    task model

---
+++ KR/models/task.rb
@@ -0,0 +1,4 @@
+class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :status, inclusion: { in: %w[в очікуванні в роботі завершені], message: "%{value} недопустимий статус" }
+end
